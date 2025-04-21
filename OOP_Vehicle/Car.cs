157a0b20fcf57365d6b9bda7ae7fdd2f3605a306
SamuelLeiva <leiva20152@gmail.com>
2025-04-21 13:07:50 -0500

    Class car added

--- OOP_Vehicle/Car.cs
+++ OOP_Vehicle/Car.cs
@@ -7,22 +7,31 @@ using System.Threading.Tasks;
 namespace OOP_Vehicle;
 public class Car : ILandVehicle
 {
-    public int NumWheels => throw new NotImplementedException();
+    public int NumWheels {  get; protected set; }
+    public string NumPlate {  get; protected set; }
+    public string Id {  get; protected set; }
+    public string Producer {  get; protected set; }
 
-    public string NumPlate => throw new NotImplementedException();
-
-    public string Id => throw new NotImplementedException();
-
-    public string Producer => throw new NotImplementedException();
+    public Car(int numWheels, string numPlate, string id, string producer)
+    {
+        NumWheels = numWheels;
+        NumPlate = numPlate;
+        Id = id;
+        Producer = producer;
+    }
 
     public void Honk()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Hooonnnnk hooonk!!");
     }
 
     public void PrintDetails()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Datos del auto");
+        Console.WriteLine($"Id de vehículo: {Id}");
+        Console.WriteLine($"Productor: {Producer}");
+        Console.WriteLine($"Número de placa: {NumPlate}");
+        Console.WriteLine($"Número de ruedas: {NumWheels}");
     }
 }
 
